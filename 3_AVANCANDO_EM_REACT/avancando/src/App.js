fec3b7f00da9045c922d9f8eedd1bc6e182bab22
Miguel Antunes da Costa <miguel.costa@hengel.dominio.com.br>
2023-05-09 12:54:39 -0300

    Renderização de lista em componente

--- 3_AVANCANDO_EM_REACT/avancando/src/App.js
+++ 3_AVANCANDO_EM_REACT/avancando/src/App.js
@@ -13,6 +13,13 @@ function App() {
   const [userName] = useState("Maria")
   const idade = 15;
 
+  const cars = [
+    { id: 1, brand: "Ferrari", color: "Amarela", newCar: true, km: 0 },
+    { id: 2, brand: "KIA", color: "Branco", newCar: false, km: 34343 },
+    { id: 3, brand: "Renault", color: "Azul", newCar: false, km: 234 }
+
+  ]
+
   return (
     <div className="App">
       <h1>Avançando em React</h1>
@@ -31,6 +38,14 @@ function App() {
         <CarDetails brand="VW" km={10000} color="Azul" newCar={false} />
         <CarDetails brand="Ford" km={0} color="Azul" newCar={true} />
         <CarDetails brand="Fiat" km={4500} color="Azul" newCar={false} />
+
+        {cars.map(
+          (car) => (
+            <CarDetails brand={car.brand}
+              color={car.color}
+              km={car.km}
+              newCar={car.newCar} />)
+        )}
       </div>
     </div>
   );
