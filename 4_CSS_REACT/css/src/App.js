8312372fee8206d9ffb391ae596afc2b9ff4c2de
Miguel Antunes da Costa <miguel.costa@hengel.dominio.com.br>
2023-05-12 16:35:10 -0300

    Criação do componete MyComponent (Css de Componente)

--- 4_CSS_REACT/css/src/App.js
+++ 4_CSS_REACT/css/src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import MyComponent from './components/MyComponent';
 
 function App() {
   return (
     <div className="App">
+      {/*CSS Global */}
       <h1>React com CSS</h1>
+
+
+      {/*CSS do Componente */}
+      <MyComponent />
+      <p>Este parágrafo é do App.js</p>
     </div>
   );
 }
---
+++ 4_CSS_REACT/css/src/components/MyComponent.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './MyComponent.css'
+
+const MyComponent = () => {
+    return (
+        <div>
+            <h1>CSS de Componente</h1>
+            <p>Este é o parágrafo do Componente</p>
+            <p className="my-comp-paragraph">
+                Este tb é do componente
+            </p>
+        </div>
+    )
+}
+
+export default MyComponent;
